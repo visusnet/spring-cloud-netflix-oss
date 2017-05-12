fc4d9545d98aae13d4a2bb333ddb541ced1ba0ce
Alexander Müller <alexander.mueller01@metro.de>
2017-05-12 12:05:46 +0200

    Removed @DiscoveryClient.

--- rest-consumer/src/main/java/de/codecentric/RestConsumerApplication.java
+++ rest-consumer/src/main/java/de/codecentric/RestConsumerApplication.java
@@ -2,7 +2,6 @@ package de.codecentric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;

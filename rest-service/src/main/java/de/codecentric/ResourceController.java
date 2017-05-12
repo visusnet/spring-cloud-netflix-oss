f4fa9adf7bf7db32f7c6c5f3621f9577fec0b839
Alexander Müller <alexander.mueller01@metro.de>
2017-05-12 10:58:34 +0200

    Removed unused import.

--- rest-service/src/main/java/de/codecentric/ResourceController.java
+++ rest-service/src/main/java/de/codecentric/ResourceController.java
@@ -2,7 +2,6 @@ package de.codecentric;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ResourceController {
